732f44ea49aa71a785d0a4ad14a2a346df793574
Rob Tomlin <rob.p.tomlin@icloud.com>
2026-02-26 16:14:27 +0000

    example of getting the websocket token

--- examples/private_method.cpp
+++ examples/private_method.cpp
@@ -6,28 +6,24 @@
 using namespace std;
 using namespace Kraken;
 
-constexpr char api_key[] = "your API key here";
-constexpr char api_private_key[] = "your API private key here";
-
-int main()
+int main(int argc, char* argv[])
 {
     curl_global_init(CURL_GLOBAL_ALL);
+
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <api_key> <api_private_key>" << endl;
+        return 1;
+    }
+
+    auto api_key = string(argv[1]);
+    auto api_private_key = string(argv[2]);
+    cout << "API Key: " << api_key << endl;
+    cout << "API Private Key: " << api_private_key << endl;
 
     try {
         KAPI kapi(api_key, api_private_key);
         KAPI::Input in;
-
-        in.insert(make_pair("pair", "XXBTZUSD"));
-        in.insert(make_pair("type", "buy"));
-        in.insert(make_pair("ordertype", "limit"));
-        in.insert(make_pair("price", "45000.1"));
-        in.insert(make_pair("volume", "2.1234"));
-        in.insert(make_pair("leverage", "2:1"));
-        in.insert(make_pair("close[ordertype]", "stop-loss-limit"));
-        in.insert(make_pair("close[price]", "38000"));
-        in.insert(make_pair("close[price2]", "36000"));
-
-        cout << kapi.private_method("AddOrder", in) << endl;
+        cout << kapi.private_method("GetWebSocketsToken", in) << endl;
     }
     catch(exception& e) {
         cerr << "Error: " << e.what() << endl;
