175d46d798f584be9d3a0ede8969275addbf8b85
Rob Tomlin <46629570+tomlin256@users.noreply.github.com>
2026-02-28 21:15:25 +0000

    cast

--- tests/examples/public_ws.cpp
+++ tests/examples/public_ws.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
             auto json = nlohmann::json::parse(msg->str);
             if (json.contains("method"))
             {
-               spdlog::info("{}: {}", json["method"], json["success"] ? "success" : "failure");
+               spdlog::info("{}: {}", json["method"], json["success"].get<bool>() ? "success" : "failure");
             }
             else if (json.contains("channel")) 
             {
