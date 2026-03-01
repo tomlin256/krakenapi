23dcbcb1d2ef4d08159848e875b6b9bd72de4759
Rob Tomlin <rob.p.tomlin@icloud.com>
2026-03-01 17:01:05 +0000

    use keys from home dir

--- tests/examples/private_rest.cpp
+++ tests/examples/private_rest.cpp
@@ -10,18 +10,10 @@ int main(int argc, char* argv[])
 {
     curl_global_init(CURL_GLOBAL_ALL);
 
-    if (argc != 3) {
-        cerr << "Usage: " << argv[0] << " <api_key> <api_private_key>" << endl;
-        return 1;
-    }
-
-    auto api_key = string(argv[1]);
-    auto api_private_key = string(argv[2]);
-    cout << "API Key: " << api_key << endl;
-    cout << "API Private Key: " << api_private_key << endl;
+    auto keys = load_keys("default");
 
     try {
-        KAPI kapi(api_key, api_private_key);
+        KAPI kapi(keys.apiKey, keys.privateKey);
         KAPI::Input in;
         cout << kapi.private_method("GetWebSocketsToken", in) << endl;
     }
